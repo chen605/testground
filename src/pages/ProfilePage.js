ba7d5a7355fdc539cdb0010cd260c80801a92274
chen605 <chow_chen_1996@hotmail.com>
2021-04-14 14:16:00 +0200

    added logout button

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "../styles/ProfilePage.css";
+import { auth } from "../firebase/firebase";
 
 function ProfilePage() {
   return (
     <div>
       <h1>Profile page</h1>
+      <button onClick={() => auth.signOut()} className="profileScreen__signOut">
+        Sign Out
+      </button>
     </div>
   );
 }
